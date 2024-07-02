7f013cdd86cffb7fd52e93f1007c654edc8bfb12
kevglass <kevin@rune.ai>
2024-07-02 14:07:00 +0100

    Update pokemon theme

--- duplicates.js
+++ duplicates.js
@@ -10,19 +10,19 @@ import * as fs from 'fs';
         const questions = JSON.parse(fs.readFileSync(THEME+"/questions_"+lang+".json"));
         const output = [];
         for (const q of questions) {
-            if (texts.includes(q.question+"-"+q.correct_answer)) {
+            if (texts.includes(q.correct_answer)) {
 
                 continue;
             }
             output.push(q);
-            texts.push(q.question+"-"+q.correct_answer);
+            texts.push(q.correct_answer);
         }
-        console.log(lang+" Removed " + (questions.length - output.length) + " duplicates");
+        console.log(lang+" Removed " + (questions.length - output.length) + " duplicates - " + output.length + " questions remaining");
         fs.writeFileSync(THEME+"/questions_"+lang+".json", JSON.stringify(output, null, 2));
     }
 
     await processLang("en");
-    await processLang("ru");
-    await processLang("es");
-    await processLang("pt");
+    // await processLang("ru");
+    // await processLang("es");
+    // await processLang("pt");
 })();
